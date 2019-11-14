64ec73c3c591b47904d8be48ef8a32851275b74d
Duane Roelands <duane.roelands@gmail.com>
2019-11-13 23:18:37 -0500

    Initial commit

---
+++ xl2md/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtExcelWorkbook = New System.Windows.Forms.TextBox()
+        Me.btnSelectWorkbook = New System.Windows.Forms.Button()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtExcelWorkbook
+        '
+        Me.txtExcelWorkbook.Location = New System.Drawing.Point(156, 11)
+        Me.txtExcelWorkbook.Name = "txtExcelWorkbook"
+        Me.txtExcelWorkbook.Size = New System.Drawing.Size(463, 20)
+        Me.txtExcelWorkbook.TabIndex = 0
+        '
+        'btnSelectWorkbook
+        '
+        Me.btnSelectWorkbook.Location = New System.Drawing.Point(12, 11)
+        Me.btnSelectWorkbook.Name = "btnSelectWorkbook"
+        Me.btnSelectWorkbook.Size = New System.Drawing.Size(138, 21)
+        Me.btnSelectWorkbook.TabIndex = 1
+        Me.btnSelectWorkbook.Text = "Select Excel Workbook"
+        Me.btnSelectWorkbook.UseVisualStyleBackColor = True
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(623, 12)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(68, 21)
+        Me.btnGo.TabIndex = 2
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(703, 43)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.btnSelectWorkbook)
+        Me.Controls.Add(Me.txtExcelWorkbook)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "XL2MD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtExcelWorkbook As TextBox
+    Friend WithEvents btnSelectWorkbook As Button
+    Friend WithEvents btnGo As Button
+End Class
---
+++ xl2md/Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports OfficeOpenXml
+Public Class Form1
+    Private Sub btnSelectWorkbook_Click(sender As Object, e As EventArgs) Handles btnSelectWorkbook.Click
+        Dim dlg As New OpenFileDialog
+        dlg.RestoreDirectory = True
+        dlg.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyDocuments
+        If dlg.ShowDialog = DialogResult.OK Then
+            Me.txtExcelWorkbook.Text = dlg.FileName
+        End If
+    End Sub
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        Dim ExcelFileInfo As New System.IO.FileInfo(Me.txtExcelWorkbook.Text)
+        Using package As New ExcelPackage(ExcelFileInfo)
+            For Each worksheet As ExcelWorksheet In package.Workbook.Worksheets
+                If worksheet.Name.StartsWith("_") Then
+                    '' skip this sheet
+                Else
+                    Dim Headers As New List(Of String)
+                    Dim columnNumbers As New List(Of Integer)
+                    Dim columnNumber As Integer = 1
+                    Do While worksheet.Cells(1, columnNumber).Value IsNot Nothing
+                        If worksheet.Cells(1, columnNumber).Value.ToString.StartsWith("_") Then
+                            '' skip this column
+                        Else
+                            Headers.Add(worksheet.Cells(1, columnNumber).Value.ToString)
+                            columnNumbers.Add(columnNumber)
+                        End If
+                        columnNumber += 1
+                    Loop
+
+                    Dim markdownTableHeader As New System.Text.StringBuilder
+                    Dim markdownTableSpacer As New System.Text.StringBuilder
+                    For Each header As String In Headers
+                        If markdownTableHeader.Length > 0 Then
+                            markdownTableHeader.Append(" | ")
+                            markdownTableSpacer.Append("-|-")
+                        End If
+                        markdownTableHeader.Append(header)
+                        markdownTableSpacer.Append("-----")
+                    Next
+
+                    Dim markdownTableData As New System.Text.StringBuilder
+                    Dim rowNumber As Integer = 2
+                    Do While worksheet.Cells(rowNumber, 1).Value IsNot Nothing
+                        Dim markDownRow As New System.Text.StringBuilder
+                        For Each c As Integer In columnNumbers
+                            If markDownRow.Length > 0 Then
+                                markDownRow.Append(" | ")
+                            End If
+                            If worksheet.Cells(rowNumber, c).Value IsNot Nothing Then
+                                markDownRow.Append(worksheet.Cells(rowNumber, c).Value)
+                            Else
+                                markDownRow.Append(" ")
+                            End If
+                        Next
+                        markdownTableData.AppendLine(markDownRow.ToString)
+                        rowNumber += 1
+                    Loop
+
+                    Dim filename As String = worksheet.Name & ".txt"
+                    Using sw As New System.IO.StreamWriter(filename, False)
+                        sw.WriteLine(markdownTableHeader.ToString)
+                        sw.WriteLine(markdownTableSpacer.ToString)
+                        sw.WriteLine(markdownTableData.ToString)
+                    End Using
+                    Process.Start(filename)
+
+                End If
+            Next
+        End Using
+
+
+    End Sub
+
+
+
+
+
+
+End Class
